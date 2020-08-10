da585b7935b7331d5183cb95ebf1474028959f57
이현호 <mizzking75@gmail.com>
2020-08-10 15:16:27 +0900

    Add tabbar

---
+++ githubFollwer/FavoritesListVC.swift
@@ -0,0 +1,19 @@
+//
+//  FavoritesListVS.swift
+//  githubFollwer
+//
+//  Created by 이현호 on 2020/08/08.
+//  Copyright © 2020 tempYsoup. All rights reserved.
+//
+
+import UIKit
+
+class FavoritesListVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBlue
+    }
+
+
+}
---
+++ githubFollwer/GFButton.swift
@@ -0,0 +1,17 @@
+//
+//  GFButton.swift
+//  githubFollwer
+//
+//  Created by 이현호 on 2020/08/10.
+//  Copyright © 2020 tempYsoup. All rights reserved.
+//
+
+import UIKit
+
+class GFButton: UIButton {
+//
+//    override init(frame: CGRect) {
+//        super.init(frame: frame)
+//    }
+
+}
