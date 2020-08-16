a2e3a4c159e9ae9b9fe96a64531f0e4193bd84b9
hyunholee <mizzking75@gmail.com>
2020-08-17 01:42:10 +0900

    Add constraint of button

--- githubFollwer/GFButton.swift
+++ githubFollwer/GFButton.swift
@@ -9,9 +9,29 @@
 import UIKit
 
 class GFButton: UIButton {
-//
-//    override init(frame: CGRect) {
-//        super.init(frame: frame)
-//    }
-
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    // inited by storyboard
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    init(backgroundColor: UIColor, title: String) {
+        super.init(frame: .zero)
+        self.backgroundColor = backgroundColor
+        self.setTitle(title, for: .normal)
+        configure()
+    }
+    
+    // I don't want people configure this button outside
+    private func configure() {
+        layer.cornerRadius = 10
+        titleLabel?.textColor = .white
+        titleLabel?.font = UIFont.preferredFont(forTextStyle: .headline)
+        // using autolayout
+        translatesAutoresizingMaskIntoConstraints = false
+    }
 }
--- githubFollwer/SearchVC.swift
+++ githubFollwer/SearchVC.swift
@@ -10,21 +10,63 @@ import UIKit
 
 class SearchVC: UIViewController {
 
+    let logoImageView = UIImageView()
+    let usernameTextField = GFTextField()
+    let callToActionButton = GFButton(backgroundColor: .systemGreen, title: "Get Followers")
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemPink
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemBackground
+        configureLogoImageView()
+        configureTextField()
+        configureCallToActionButton()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    // viewDidload에서 부르면 처음에는 제대로 동작하지만 다시 뷰가 불릴 때는 불리지 않기 때문에 뷰에 모습이 계속 적용되길 바란다면 viewWillappear
+    override func viewWillAppear(_ animated: Bool) {
+        // super를 부르지 않는 경우는?
+        super.viewWillAppear(animated)
+        navigationController?.isNavigationBarHidden = true
     }
-    */
+    
+    private func configureLogoImageView() {
+        view.addSubview(logoImageView)
+        // 커스텀 버튼과 텍스트 필드는 안에 코드가 들어있지만 레귤러 이미지 뷰는 들어있지 않는다.
+        logoImageView.translatesAutoresizingMaskIntoConstraints = false
+        // 문자열이 들어가 있다면 오타와 이름변경으로 인해 언젠가 크래시를 낼 위험을 잠정적으로 가지고 있는다.
+        logoImageView.image = UIImage(named: "gh-logo")!
+        
+        NSLayoutConstraint.activate([
+            logoImageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 80),
+            logoImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            logoImageView.heightAnchor.constraint(equalToConstant: 200),
+            logoImageView.widthAnchor.constraint(equalToConstant: 200)
+        ])
+        
+    }
+    
+    private func configureTextField() {
+        view.addSubview(usernameTextField)
+        
+        NSLayoutConstraint.activate([
+            usernameTextField.topAnchor.constraint(equalTo: logoImageView.bottomAnchor, constant: 48),
+            usernameTextField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 50),
+            usernameTextField.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -50),
+            usernameTextField.heightAnchor.constraint(equalToConstant: 50)
+        ])
+    }
+    
+    private func configureCallToActionButton() {
+        view.addSubview(callToActionButton)
+        
+        NSLayoutConstraint.activate([
+            callToActionButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -50),
+            callToActionButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 50),
+            callToActionButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -50),
+            callToActionButton.heightAnchor.constraint(equalToConstant: 50)
+        ])
+    }
+    
 
 }
