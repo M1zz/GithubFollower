21f78172ed2e61e24cdd843712be47526d7ac0e6
hyunholee <mizzking75@gmail.com>
2020-08-17 00:16:26 +0900

    Add custom button, textField

---
+++ githubFollwer/GFTextField.swift
@@ -0,0 +1,21 @@
+//
+//  GFTextField.swift
+//  githubFollwer
+//
+//  Created by hyunho lee on 2020/08/16.
+//  Copyright © 2020 tempYsoup. All rights reserved.
+//
+
+import UIKit
+
+class GFTextField: UITextField {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
