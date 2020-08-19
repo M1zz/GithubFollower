f452410a6e4577d01c16e2ccae17b2b4c9255951
hyunholee <mizzking75@gmail.com>
2020-08-20 00:13:36 +0900

    Add networkManager

---
+++ githubFollwer/Model/Follower.swift
@@ -0,0 +1,9 @@
+//
+//  Follower.swift
+//  githubFollwer
+//
+//  Created by hyunho lee on 2020/08/19.
+//  Copyright © 2020 tempYsoup. All rights reserved.
+//
+
+import Foundation
---
+++ githubFollwer/Model/User.swift
@@ -0,0 +1,9 @@
+//
+//  User.swift
+//  githubFollwer
+//
+//  Created by hyunho lee on 2020/08/19.
+//  Copyright © 2020 tempYsoup. All rights reserved.
+//
+
+import Foundation
