6a240ff46e14e6ab990d1715959f06560de5a2ae
hyunho.lee <mizzking75@gmail.com>
2021-02-14 00:33:33 +0900

    Add FollowerCell, GFImageView

---
+++ githubFollwer/Custom Views/GFAvatarImageView.swift
@@ -0,0 +1,32 @@
+//
+//  GFAvatarImageView.swift
+//  githubFollwer
+//
+//  Created by 이현호 on 2021/02/10.
+//  Copyright © 2021 tempYsoup. All rights reserved.
+//
+
+import UIKit
+
+class GFAvatarImageView: UIImageView {
+
+    let placeholderImage = UIImage(named: "avatar-placeholder")!
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func configure() {
+        layer.cornerRadius = 10
+        clipsToBounds = true
+        image = placeholderImage
+        translatesAutoresizingMaskIntoConstraints = false
+        
+    }
+    
+}
