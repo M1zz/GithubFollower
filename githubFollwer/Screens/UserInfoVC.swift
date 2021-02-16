ffdd75c40e8cd0b41c27c306b5d2bdd8e7785840
hyunho.lee <mizzking75@gmail.com>
2021-02-16 23:34:36 +0900

    Add userInfoViewController

--- githubFollwer/Screens/UserInfoVC.swift
+++ githubFollwer/Screens/UserInfoVC.swift
@@ -10,26 +10,52 @@ import UIKit
 
 class UserInfoVC: UIViewController {
 
+    let headerView = UIView()
+    
     var username: String!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         view.backgroundColor = .systemBackground
         let doneButton = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(dissmissVC))
         navigationItem.rightBarButtonItem = doneButton
+        
+        layoutUI()
         
         NetworkManager.shared.getUserInfo(for: username) { [weak self] result in
             guard let self = self else { return }
             
             switch result {
             case .success(let user):
-                print(user)
-                
+                DispatchQueue.main.async {
+                    self.add(childVC: GFUserInfoHeaderVC(user: user), to: self.headerView)
+                }
+
             case .failure(let error):
                 self.presentGFAlertOnMainThread(title: "무언가 잘못 되었습니다.", message: error.rawValue, buttonTitle: "Ok")
             }
             
         }
+    }
+    
+    func layoutUI() {
+        view.addSubview(headerView)
+        headerView.translatesAutoresizingMaskIntoConstraints = false
+
+        NSLayoutConstraint.activate([
+            headerView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            headerView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            headerView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            headerView.heightAnchor.constraint(equalToConstant: 180)
+        ])
+    }
+    
+    func add(childVC: UIViewController, to containerView: UIView) {
+        addChild(childVC)
+        containerView.addSubview(childVC.view)
+        childVC.view.frame = containerView.bounds
+        childVC.didMove(toParent: self)
     }
     
     @objc func dissmissVC() {
