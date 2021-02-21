bb8ad90a2d8c730944a013cabe9e2568f4c513c8
hyunho.lee <mizzking75@gmail.com>
2021-02-21 21:41:13 +0900

    refactor

--- githubFollwer/Custom Views/Labels/GFSecndaryTitleLabel.swift
+++ githubFollwer/Custom Views/Labels/GFSecndaryTitleLabel.swift
@@ -20,10 +20,9 @@ class GFSecndaryTitleLabel: UILabel {
     }
     
     
-    init(fontsize: CGFloat) {
-        super.init(frame : .zero)
+    convenience init(fontsize: CGFloat) {
+        self.init(frame : .zero)
         font = UIFont.systemFont(ofSize: fontsize, weight: .medium)
-        configure()
     }
     
     
